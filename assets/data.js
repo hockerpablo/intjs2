1878e035f2d278ac376e89a761fcfcc3e382007f
pablo <hockeralma22@gmail.com>
2023-08-24 22:41:06 -0300

    main

---
+++ assets/data.js
@@ -0,0 +1,65 @@
+const prductsData = [
+    {
+        id: 1,
+        name: "Apple Macbook Air (13 Pulgadas, 2020, Chip M1, 256 Gb De Ssd, 8 Gb De Ram) – 3 Colores",
+        value: "750.000", 
+        category: "notebooks",
+        cardImg: "./assets/img/products/macbook air 13/1.jpg",
+    },
+    {
+        id: 2,
+        name: "Lenovo Legion Y540 intel i7-9750H 256gb- RAM16gb NVIDIA GeForce GTX 1660Ti",
+        value: "975.000",
+        category:"notebooks",
+        cardImg: "./assets/img/products/lenovo legion/1.jpg",
+    },
+    {
+        id: 3,
+        name: "Asus TUF A15 AMD Ryzen 7 4800H 512gb-ram16gb NVIDIA GeForce RTX 3050TI",
+        value: "1.096.000",
+        category: "notebooks",
+        cardImg:"./assets/img/products/asus tuf f15/1.jpg",
+    },
+    {
+        id: 4,
+        name:"ASUS TUF Gaming NVIDIA GeForce RTX™ 4070 Ti OC Edition",
+        value:"799.000",
+        category:"Placas de video",
+        cardImg:"./assets/img/products/asus tuf rtx 4070ti/1.jpg",
+    },
+    {
+        id: 5,
+        name: "MSI Gaming GeForce RTX 4070 Ti 12GB (RTX 4070 Ti Ventus 3X 12G OC)",
+        value: "815.000",
+        category:"Placas de video",
+        cardImg:"./assets/img/products/msi 4070ti/1.jpg",
+    },
+    {
+        id: 6,
+        name: "ASUS ROG Strix GeForce RTX® 4090 OC Edition",
+        value: "2.230.000",
+        category:"Placas de video",
+        cardImg:"./assets/img/products/asus rog strix 4090/1.jpg"
+    },
+    {
+        id: 7,
+        name:"iPhone 13 256 GB - Midnight",
+        value:"1.143.000",
+        category:"Smartphones",
+        cardImg:"./assets/img/products/iphone13/1.jpg"
+    },
+    {
+        id: 8,
+        name:"Samsung Galaxy Z Fold5 Icy Blue 512gb",
+        value:"614.000",
+        category:"Smartphones",
+        cardImg:"./assets/img/products/samsung galaxy fold5/1.jpg"
+    },
+    {
+        id: 9,
+        name:"Xiaomi Redmi Note 11 Verde 128/4gb 6.43",
+        value: "132.999",
+        category:"Smartphones",
+        cardImg:"./assets/img/products/xiaomi note11/1.jpg"
+    }
+]
